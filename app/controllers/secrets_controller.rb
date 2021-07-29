f56266238091062650e4812304a1de7df856d116
bcharlotin1 <biancacharlotin1@gmail.com>
2021-07-29 14:50:35 -0400

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,13 +1,16 @@
 class SecretsController < ApplicationController
+    before_action :require_login
     def show
-        if !current_user.blank?
-            redirect_to '/login'
-        else
-            render :show
-        end
-     
+        # if current_user.blank?
+        #     redirect_to login_path
+        # else
+        #     render :show
+        # end
     end
 
-
+     private 
+    def require_login
+        return head(:forbidden) unless session.include? :name
+      end
   end
   
